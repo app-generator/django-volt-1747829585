e2d699b2246c9abf25e8b079d42c3e5f25841dfe
App Generator <51070104+app-generator@users.noreply.github.com>
2025-05-21 15:13:14 +0300

    initial commit

---
+++ home/models.py
@@ -0,0 +1,55 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    position = models.CharField(max_length=255, null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Project(models.Model):
+
+    #__Project_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Project_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Project")
+        verbose_name_plural = _("Project")
+
+
+class Task(models.Model):
+
+    #__Task_FIELDS__
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    #__Task_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Task")
+        verbose_name_plural = _("Task")
+
+
+
+#__MODELS__END
